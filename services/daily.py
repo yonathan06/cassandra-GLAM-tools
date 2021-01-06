3afb18a440ae7efc59776b8113362bdf589b64bf
Yonatan Bendahan <yonatan.bendahan@gmail.com>
2021-01-06 10:47:39 +0200

    update daily.py

--- services/daily.py
+++ services/daily.py
@@ -4,26 +4,83 @@ import os
 from datetime import date, timedelta
 from lib.sentry import with_sentry
 from etl.glams_table import get_running_glams, get_glam_database_connection, get_glam_images
-from etl.views import update_glam_mediacounts_from_file
 from etl.run import process_glam
 from etl.download_mediacounts import download_file
+import urllib.parse
+from tqdm import tqdm
+
+
+def process_glams(glams):
+    for glam in glams:
+        process_glam(glam)
+
+
+def get_file_key_from_line(line):
+    arr = line.decode().split("\t")
+    keysX = arr[0].split("/")
+    key = keysX[len(keysX) - 1]
+    key = urllib.parse.unquote(key)
+    return key, arr
+
+
+def dailyinsert_query(key, arr, date_val: date):
+    return f"SELECT * FROM dailyinsert('" + key.replace(
+        "'", "''") + "','" + date_val.strftime("%Y-%m-%d") + "'," + arr[2] + "," + arr[22] + "," + arr[23] + ")"
+
+
+def dailyinsert_glams(glams, line, date_val):
+    key, arr = get_file_key_from_line(line)
+    counter = 0
+    for glam in glams:
+        if key in glam['images']:
+            glam['cur'].execute(
+                dailyinsert_query(key, arr, date_val))
+            counter += 1
+    return counter
+
+
+def dailyinsert_from_file(glams, filepath, date_val):
+    extracted_file = bz2.BZ2File(filepath, "r")
+    total_image_num = 0
+    for glam in glams:
+        glam['conn'] = get_glam_database_connection(glam['database'])
+        glam['images'] = get_glam_images(glam['conn'])
+        glam['cur'] = glam['conn'].cursor()
+        total_image_num += len(glam['images'])
+    with tqdm(total=total_image_num) as bar:
+        counter = 0
+        for line in extracted_file:
+            inserted_counter = dailyinsert_glams(glams, line, date_val)
+            counter += inserted_counter
+            bar.update(inserted_counter)
+            if counter == total_image_num:
+                logging.info(
+                    f"Counter {counter} total_image_num{total_image_num}")
+                break
+    extracted_file.close()
+
+
+def refresh_and_close(glams):
+    logging.info(f"refrashing views for {len(glams)} glams")
+    for glam in glams:
+        logging.info(f"refrashing for {glam['name']}")
+        glam['cur'].execute('REFRESH MATERIALIZED VIEW visualizations_sum')
+        glam['cur'].execute('REFRESH MATERIALIZED VIEW visualizations_stats')
+        glam['cur'].close()
+        glam['conn'].close()
+
+
+def main():
+    yesterday_date = date.today() - timedelta(days=1)
+    filepath = download_file(yesterday_date)
+    glams = get_running_glams()
+    process_glams(glams)
+    dailyinsert_from_file(glams, filepath, yesterday_date)
+    refresh_and_close(glams)
+    os.remove(filepath)
+
 
 if __name__ == "__main__":
     logging.info("Starting daily tasks")
     with_sentry()
-    yesterday_date = date.today() - timedelta(days=1)
-    glams = get_running_glams()
-    filepath = download_file(yesterday_date)
-    extracted_file = bz2.BZ2File(filepath, "r")
-
-    for glam in glams:
-        process_glam(glam)
-        conn = get_glam_database_connection(glam['database'])
-        glam_images = get_glam_images(conn)
-        extracted_file.seek(0)
-        update_glam_mediacounts_from_file(
-            extracted_file, yesterday_date, conn, glam_images, glam["name"])
-        conn.close()
-
-    extracted_file.close()
-    os.remove(filepath)
+    main()
