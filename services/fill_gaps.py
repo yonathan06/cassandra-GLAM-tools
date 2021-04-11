a27b94b61e2a00a31d3200f53f6d028db7acf0d0
Yonatan Bendahan <yonatan.bendahan@gmail.com>
2021-04-11 16:50:08 +0300

    fill_gaps with multiple glams

--- services/fill_gaps.py
+++ services/fill_gaps.py
@@ -1,16 +1,17 @@
 from etl.mediacounts_dump import dailyinsert_from_file
 import logging
 from etl.s3 import get_mediacount_file_by_date
-import sys
-
+from typing import List
+import argparse
 from datetime import date, datetime, timedelta
 from etl.glams_table import close_glams_connections, get_glams, load_glams_images, open_glams_connections, refresh_glams_visualizations
 
 
-def _main(start_date: date, end_date: date, glam_name: str):
+def _main(start_date: date, end_date: date, glams_names: List[str]):
     glams = get_glams()
-    if glam_name != None:
-        glams = list(filter(lambda g: g['name'] == glam_name, glams))
+    if glams_names != None:
+        glams = list(
+            filter(lambda g:  g['name'] in glams_names, glams))
     if len(glams) == 0:
         logging.info('There are no glams to process')
         return
@@ -30,12 +31,16 @@ def _main(start_date: date, end_date: date, glam_name: str):
 
 
 if __name__ == '__main__':
-    start_date = datetime.strptime(sys.argv[1], "%Y-%m-%d").date()
-    end_date = date.today()
-    glam_name = None
-    if len(sys.argv) > 2:
-        end_date = datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
-    if len(sys.argv) > 3:
-        glam_name = sys.argv[3]
-    _main(start_date, end_date=end_date, glam_name=glam_name)
+    parser = argparse.ArgumentParser(
+        description="fill gaps of mediacounts for glams from a given start date")
+    parser.add_argument("--start_date", type=lambda s: datetime.strptime(
+        s, '%Y-%m-%d').date(), help="Start date (format Y-m-d)", required=True)
+    parser.add_argument(
+        "--end_date", type=lambda s: datetime.strptime(
+            s, '%Y-%m-%d').date(), help="End date, default to today (format Y-m-d)", default=date.today())
+    parser.add_argument("--glams", nargs='+', type=str,
+                        help="Glam names to process (deafult to all)")
+    args = parser.parse_args()
+
+    _main(args.start_date, end_date=args.end_date, glams_names=args.glams)
     logging.info(f"Fill gaps done")
