98e6b8da2e5fbbf906b5a70358e26ddef717ca07
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2022-06-30 09:55:24 +0300

    use toolforge new url

--- services/start_services.sh
+++ services/start_services.sh
@@ -1,5 +1,5 @@
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
-autossh -f -N wmflabs
+autossh -f -N toolforge
 pm2 start new_glam_listener.py --interpreter python3 --no-autorestart
 pm2 start daily.py --cron '0 4 * * *' --interpreter python3 --no-autorestart
