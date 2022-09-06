a1a765bc7b12bb254956b0ea1c1ae944707a504d
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2022-09-06 19:12:07 +0300

    update cron time

--- services/start_services.sh
+++ services/start_services.sh
@@ -2,4 +2,4 @@ eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 autossh -f -N toolforge
 pm2 start new_glam_listener.py --interpreter python3 --no-autorestart
-pm2 start daily.py --cron '0 1 * * *' --interpreter python3 --no-autorestart
+pm2 start daily.py --cron '0 2 * * *' --interpreter python3 --no-autorestart
