9caf2282a6d46490dd847e8b8210b8a7d2d52ca4
MoriyaNadav <mn0527119313@serviceapp.glamwikidashboard.eqiad1.wikimedia.cloud>
2023-05-10 09:35:11 +0000

    logs changes

--- services/initdaily.sh
+++ services/initdaily.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 python3 $HOME/cassandra-GLAM-tools/services/daily.py  -e production
-
-
-curl -X POST http://serviceapp.glamwikidashboard.eqiad1.wikimedia.cloud:8080/ -H 'Content-Type: application/json' -d '{"login":"my_login","password":"my_password"}'
