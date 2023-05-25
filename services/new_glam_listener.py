39b561bd49cfcc61b08d4282b4d1541738912d40
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2023-05-25 16:18:30 +0300

    with get function

--- services/new_glam_listener.py
+++ services/new_glam_listener.py
@@ -66,6 +66,10 @@ def _process_mediacounts(glams):
 
 
 class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
     def do_POST(self):
         length = int(self.headers.get('content-length'))
         message = json.loads(self.rfile.read(length))
